b6944c8545aa4c786821e68c50fc729f46eea9ca
soraiayugulis <sys.yugulis@gmail.com>
2026-04-28 12:31:28 -0300

    feat: add PWA support for mobile installation

---
+++ public/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'blowbots-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/src/main.ts'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
